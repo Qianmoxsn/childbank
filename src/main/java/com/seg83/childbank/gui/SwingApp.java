a5d29e36d456831a5791a441aab7973da4e8abd6
Qianmo <92790790+Qianmoxsn@users.noreply.github.com>
2024-05-07 19:12:34 +0800

    doc: javadoc of xxxx

--- src/main/java/com/seg83/childbank/gui/SwingApp.java
+++ src/main/java/com/seg83/childbank/gui/SwingApp.java
@@ -8,13 +8,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class SwingApp {
 
+    /**
+     * Main frame of the application
+     */
     private final MainFrame mainFrame;
 
+    /**
+     * Constructor
+     * @param frame Main frame of the application
+     */
     @Autowired
     public SwingApp(MainFrame frame) {
         this.mainFrame = frame;
     }
 
+    /**
+     * Create the UI
+     */
     public void createUI() {
         mainFrame.init();
         mainFrame.setVisible(true);
