edc9869b24da2d28f52ba39fdc8fa3bd95a84d19
pangxuan945 <102933048+pangxuan945@users.noreply.github.com>
2024-05-16 22:09:06 +0800

    [UI] Added 2 new pops and home page fix
    New pops: System settings and Fixed deposit. And added a new button in home page to differentiate fixed and current deposit.

---
+++ src/main/java/com/seg83/childbank/gui/component/currentpop/FixedDepPop.java
@@ -0,0 +1,25 @@
+package com.seg83.childbank.gui.component.currentpop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+/**
+ * Represents a dialog window for System Settings.
+ */
+@Component
+@Slf4j
+
+public class FixedDepPop {
+    private JPanel panel1;
+    private JButton backButton;
+    private JButton withdrawalButton;
+    private JButton withdrawalButton1;
+    private JButton withdrawalButton2;
+    private JButton withdrawalButton3;
+    private JButton withdrawalButton4;
+    private JButton withdrawalButton5;
+    private JButton withdrawalButton6;
+}
