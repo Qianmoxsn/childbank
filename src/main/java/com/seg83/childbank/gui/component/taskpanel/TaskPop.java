71e5270dc7002ed77406173a064e4a709b10905a
pangxuan945 <102933048+pangxuan945@users.noreply.github.com>
2024-05-23 19:59:49 +0800

    [UI]New task and reward panel and pop

---
+++ src/main/java/com/seg83/childbank/gui/component/taskpanel/TaskPop.java
@@ -0,0 +1,71 @@
+package com.seg83.childbank.gui.component.taskpanel;
+
+import javax.swing.*;
+import java.awt.event.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+/**
+ * Pop of Confirming Tasks
+ */
+@Component
+@Slf4j
+public class TaskPop extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+
+    public TaskPop() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        TaskPop dialog = new TaskPop();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
