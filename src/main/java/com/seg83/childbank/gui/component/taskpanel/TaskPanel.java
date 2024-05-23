2d47990b3b6a9cf7c3d5b388c212dd8143e1bf04
pangxuan945 <102933048+pangxuan945@users.noreply.github.com>
2024-05-23 17:21:28 +0800

    [UI]New task and reward panel

---
+++ src/main/java/com/seg83/childbank/gui/component/taskpanel/TaskPanel.java
@@ -0,0 +1,17 @@
+package com.seg83.childbank.gui.component.taskpanel;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+/**
+ * Panel of Tasks
+ */
+@Component
+@Slf4j
+public class TaskPanel {
+    private JButton backHomeButton;
+    private JTable table1;
+    private JButton confirmTaskButton;
+}
