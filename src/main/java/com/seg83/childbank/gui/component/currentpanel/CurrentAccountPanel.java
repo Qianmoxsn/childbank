36e24415007d5b59b15f5376d92b2cc6a7b6433a
pangxuan945 <102933048+pangxuan945@users.noreply.github.com>
2024-05-23 15:16:51 +0800

    [UI]New a current account panel

---
+++ src/main/java/com/seg83/childbank/gui/component/currentpanel/CurrentAccountPanel.java
@@ -0,0 +1,26 @@
+package com.seg83.childbank.gui.component.currentpanel;
+
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+/**
+ * Panel of Current Account
+ */
+@Component
+@Slf4j
+
+public class CurrentAccountPanel {
+    private JPanel panel1;
+    private JButton backHomeButton;
+    private JButton depositButton;
+    private JButton withdrawalButton;
+    private JButton transactionHistoryButton;
+    private JButton settingButton;
+    private JButton settingButton1;
+    private JButton settingButton2;
+    private JButton transferToFixedButton;
+}
