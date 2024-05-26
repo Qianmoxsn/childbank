df8ede2542aa1f4362ce216101c9ceb60163b52e
jiecong_he <103429416+Jchhhh@users.noreply.github.com>
2024-05-26 20:47:40 +0800

    Update TaskPop.java

--- src/main/java/com/seg83/childbank/gui/component/taskpanel/TaskPop.java
+++ src/main/java/com/seg83/childbank/gui/component/taskpanel/TaskPop.java
@@ -100,6 +100,10 @@ public class TaskPop extends JDialog {
 
         // id should be a number
         try {
+            if (id <= 0) {
+                JOptionPane.showMessageDialog(this, "Task No. should be a positive number", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
             id = Long.parseLong(textField1.getText());
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(this, "Task No. should be a number", "Error", JOptionPane.ERROR_MESSAGE);
